419516d5785f8a0b8027b916ac29e801252e1e3a
Howard Kwon <howie.ykwon70@gmail.com>
2021-07-19 17:27:38 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+const multiply = num1*num2;
+
+
+
+let random = Math.ceil(Math.random());
+console.log(random);
+
+
+
+let num3 = 16
+let num4 = 6
+let mod = num3%num4;
+
+
+let max = Math.max(5, 10, 15, 20);
